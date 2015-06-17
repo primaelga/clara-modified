665d66c7cbee18db520dd179c0250f9446251e31
Riza Ramadan <riza.ramadan@sangkuriang.co.id>
2015-06-17 18:52:53 +0700

    refactor

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -8,76 +8,70 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class DataController extends Controller{
-    
-        
-        public function index()
+
+	public function index()
 	{
 		return view('index');
 	}
 	
-	 public function ReadService(Request $request,$table)
-	 {
-           
-             $take = $request->get('take');
-             $skip = $request->get('skip');
-             $sorts = $request->get('sort',array());
-             $filters = $request->get('filter')['filters'];
+	public function ReadService(Request $request,$table)
+	{
+		$take = $request->get('take');
+		$skip = $request->get('skip');
+		$sorts = $request->get('sort',array());
+		$filters = $request->get('filter')['filters'];
 
-             $query= DB::table($table);
-             foreach($sorts as $sort) {
-                 $query->orderBy($sort['field'],$sort['dir']);
-             }
-             
-             /*Filtering*/
-               if ($f = $request->get('filter')) {
-                foreach ($f['filters'] as $filter) {
-                    switch ($filter['operator']) {
-                        case 'eq': $filter['operator'] = '=';
-                            break;
-                        case 'neq': $filter['operator'] = '!=';
-                            break;
-                        case 'lt': $filter['operator'] = '<';
-                            break;
-                        case 'lte': $filter['operator'] = '<=';
-                            break;
-                        case 'gt': $filter['operator'] = '>';
-                            break;
-                        case 'gte': $filter['operator'] = '>=';
-                            break;
-                        case 'startswith':
-                            $filter['operator'] = 'LIKE';
-                            $filter['value'] = $filter['value'] . '%';
-                            break;
-                        case 'endswith':
-                            $filter['operator'] = 'LIKE';
-                            $filter['value'] = '%' . $filter['value'];
-                            break;
-                        case 'contains':
-                            $filter['operator'] = 'LIKE';
-                            $filter['value'] = '%' . $filter['value'] . '%';
-                            break;
-                        case 'doesnotcontain':
-                            $filter['operator'] = 'NOT LIKE';
-                            $filter['value'] = '%' . $filter['value'] . '%';
-                            break;
-                    }
-                    
-                     $query->where($filter['field'],$filter['operator'],$filter['value']);
-                    
-                }
-            }
-            
-            /*Sorting & Paging*/
-            $query->skip($skip)->take($take);
-            
-            
-             /*Count Records*/
-            $Data['Total'] = $query->count();
-            
-            
-            $Data['Data'] = $query->get();
-            
-            return response()->json($Data);
+		$query= DB::table($table);
+		foreach($sorts as $sort) {
+			$query->orderBy($sort['field'],$sort['dir']);
+		}
+		
+		/*Filtering*/
+		if ($f = $request->get('filter')) {
+			foreach ($f['filters'] as $filter) {
+				switch ($filter['operator']) {
+					case 'eq': $filter['operator'] = '=';
+						break;
+					case 'neq': $filter['operator'] = '!=';
+						break;
+					case 'lt': $filter['operator'] = '<';
+						break;
+					case 'lte': $filter['operator'] = '<=';
+						break;
+					case 'gt': $filter['operator'] = '>';
+						break;
+					case 'gte': $filter['operator'] = '>=';
+						break;
+					case 'startswith':
+						$filter['operator'] = 'LIKE';
+						$filter['value'] = $filter['value'] . '%';
+						break;
+					case 'endswith':
+						$filter['operator'] = 'LIKE';
+						$filter['value'] = '%' . $filter['value'];
+						break;
+					case 'contains':
+						$filter['operator'] = 'LIKE';
+						$filter['value'] = '%' . $filter['value'] . '%';
+						break;
+					case 'doesnotcontain':
+						$filter['operator'] = 'NOT LIKE';
+						$filter['value'] = '%' . $filter['value'] . '%';
+						break;
+				}
+				$query->where($filter['field'],$filter['operator'],$filter['value']);
+			}
+		}
+		
+		/*Sorting & Paging*/
+		$query->skip($skip)->take($take);
+
+		/*Count Records*/
+		$Data['Total'] = $query->count();
+		
+		$Data['Data'] = $query->get();
+		
+		return response()->json($Data);
 	 }
 	
 	public function DetailService($table, $id)
